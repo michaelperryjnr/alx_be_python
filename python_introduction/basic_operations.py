a7f44425abc49be2fa161f0f07d2cbf4613de33f
michaelperryjnr <michaelperryt97@gmail.com>
2024-12-14 11:24:49 -0800

    Created basic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+
+def sum():
+    return number1 + number2
+
+def diff():
+    return number1- number2
+
+def prod():
+    return number1 * number2
+
+print(f"Addition of {number1} and {number2} is {sum()}")
+print(f"Subtraction of {number1} and {number2} is {diff()}")
+print(f"Multiplication of {number1} and {number2} is {prod()}")
